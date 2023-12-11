bed523449c78b41f1e26de7765abd04e71a4d1ea
chinnaragamsetti <ragamsettirajkumar@gmail.com>
2023-12-11 00:47:20 +0000

    nodejs intro

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const path = require("path");
+
+const dbpath = path.join(__dirname, goodreads.db);
+
+let db = null;
+
+const initializerDbAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("server is running");
+    });
+  } catch (e) {
+    console.log(e.message);
+    process.exit(1);
+  }
+};
+
+initializerDbAndServer();
+
+app.get("/books/", async (request, response) => {
+  const getbookarray = `
+    Selection
+    *
+    FROM 
+    book
+    ORDER BY
+    book_id;`;
+  const bookarray = await db.all(getbookarray);
+  resonse.send(bookarray);
+});
